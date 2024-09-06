ce290aec5921ecdf83a913a1d5893b0f689969f2
TomOutfit <TomOutfit2020@gmail.com>
2024-09-06 14:36:27 +0700

    Up thuật toán sort

--- LinkedList_B2/src/mylinkedlist/Sort.java
+++ LinkedList_B2/src/mylinkedlist/Sort.java
@@ -1,5 +1,19 @@
 package mylinkedlist;
 
 public class Sort {
-    
+    private Node head;
+    public void Sort(){
+        if(head == null || head.next == null){
+            System.out.println("Không thể tráo đổi");
+        }
+        Node currentNode = head;
+        int i = 0;
+        while (currentNode.next != null){
+            if(currentNode.getData() > currentNode.next.getData()){
+                int temp = currentNode.getData();
+                currentNode.getData() = currentNode.next.getData();
+                temp = currentNode.next.getData();
+            }
+        }
+    }
 }
