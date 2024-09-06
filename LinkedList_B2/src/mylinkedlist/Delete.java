b9009ead0a49bf55f79193db873ba3fdc2cd6c6c
TomOutfit <TomOutfit2020@gmail.com>
2024-09-06 14:41:28 +0700

    Update code Delete

--- LinkedList_B2/src/mylinkedlist/Delete.java
+++ LinkedList_B2/src/mylinkedlist/Delete.java
@@ -1,5 +1,17 @@
 package mylinkedlist;
 
 public class Delete {
-    
+    private Node head;
+    public void Delete(){
+    if(head == null){
+        System.out.println("Không có gì để xóa hết");
+    }
+    Node temp = head;
+    count = length();
+    while(temp.next != null){
+        temp.next = head;
+        temp = null;
+        count = count - 1;
+    }
+}
 }
